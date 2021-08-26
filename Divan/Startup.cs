630e9effee5bac115513ee6aa6f9f07dd81ca89c
Bobb-93 <borisiakimov@gmail.com>
2021-08-26 11:12:45 +0300

    some changes

---
+++ Divan/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Divan.Startup))]
+namespace Divan
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
